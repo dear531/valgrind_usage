cbee3feef2afb3d3611f93a221229f4f4609d338
zhangly <dear531@21cn.com>
2014-09-08 12:28:22 +0800

    normality version show

--- memory_init.c
+++ memory_init.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define VERSION	"v0.0.1"
+void printf_version(int argc, char *argv[])
+{
+	int t;
+	while (1 < argc && -1 != (t = getopt(argc, argv, "v"))) {
+		switch (t) {
+			case 'v':
+				fprintf(stdout, "%s\n", VERSION);
+			default:
+				break;
+		}
+	}
+	return;
+}
 
 int main(int argc, char *argv[])
 {
+	printf_version(argc, argv);
+
 	int a[1];
 	a[0] = 0;
 	fprintf(stdout, "a[0]:%d\n", a[0]);
