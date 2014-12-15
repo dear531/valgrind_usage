69f4cbf935385155bf805cceaf4167f1f0319d0a
zhangly <dear531@21cn.com>
2014-12-15 11:15:23 +0800

    add fprint reference uninitialised string

--- uninit_memory.c
+++ uninit_memory.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	char memory[10];
 	int i;
 	for (i = 0; i < 10; i++) {
-		fprintf(stdout, "memory :%d, %c\n", i, memory[i]);
+		fprintf(stdout, "memory [%d]:%c\n", i, memory[i]);
 	}
 	return 0;
 }
