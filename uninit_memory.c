5d775eca6ba6c7289545d6b9cd157f43970c72c9
zhangly <dear531@21cn.com>
2014-12-15 14:34:09 +0800

    uninitailised reference for heap

--- uninit_memory.c
+++ uninit_memory.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	int m;
-
-	write(STDOUT_FILENO, &m, sizeof(m));
-	fprintf(stdout, "m:%d\n", m);
+	char *p;
+	int i;
+	p = malloc(sizeof(*p) * 10);
+	p[0] = 0;
+	for (i = 0; i < 10; i++) {
+		fprintf(stdout, "p[%d]:%c\n", i, p[i]);
+	}
+	free(p);
 
 	return 0;
 }
