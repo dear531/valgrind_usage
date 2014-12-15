7398d9738ad8d445b0fe8070e7bcef473235703a
zhangly <dear531@21cn.com>
2014-12-15 10:55:51 +0800

    use uninitialised string for fprintf

--- uninit_memory.c
+++ uninit_memory.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	char memory[10];
 	int i;
 	for (i = 0; i < 10; i++) {
-		write(STDOUT_FILENO, memory + i, sizeof(*(memory + i)));
+		fprintf(stdout, "memory :%d, %c\n", i, memory[i]);
 	}
 	return 0;
 }
