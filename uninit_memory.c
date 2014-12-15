9b2a7f3fa50eb37ff043d239f75c4077097f0935
zhangly <dear531@21cn.com>
2014-12-15 11:29:59 +0800

    reference unintialised intger

--- uninit_memory.c
+++ uninit_memory.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-	char memory[10];
-	int i;
-	for (i = 0; i < 10; i++) {
-		fprintf(stdout, "memory [%d]:%c\n", i, memory[i]);
-	}
+	int m;
+
+	write(STDOUT_FILENO, &m, sizeof(m));
+	fprintf(stdout, "m:%d\n", m);
+
 	return 0;
 }
