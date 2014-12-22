25641e32b837f7089d71f4d11d51b74ad1877d05
zhangly <dear531@21cn.com>
2014-12-22 11:28:54 +0800

    add show graph detail of stack

--- massif/massif.c
+++ massif/massif.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int func(int num)
 {
 	char *p = NULL;
-	int i = 0;
 	p = malloc(sizeof(*p) * 10);
-	for (i = 0; 10 > i; i++) {
-		p[i] = '0' + i;
-		fprintf(stdout, "p[%d]:%c\n", i, p[i]);
+	if (0 == num) {
+		int i = 0;
+		for (i = 0; 10 > i; i++) {
+			p[i] = '0' + i;
+			fprintf(stdout, "p[%d]:%c\n", i, p[i]);
+		}
+	} else {
+		func(num - 1);
 	}
 	free(p);
-	
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	func(6);
 	return 0;
 }
